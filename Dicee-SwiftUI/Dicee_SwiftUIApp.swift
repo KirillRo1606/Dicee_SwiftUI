c48530d815d367c1d95bd4b569e7fa148acb2d44
KirillRo1606 <kirill1606@gmail.com>
2023-09-30 16:26:46 +0300

    feat: add project

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Kirill Romanov on 19.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
